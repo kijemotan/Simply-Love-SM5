42f5ff43bef326c51d2f8023c4446622a99adfa6
the motan <kijemotan@gmail.com>
2025-08-22 02:18:28 +0200

    Hit me!

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - bnuy gif
 - replaced difficulty names with piu pro equivalents
 - changed some strings in the evaluation screen
+- replaced menu loop with the one from Pump It Up: The 2nd Dance Floor
--- Sounds/_common menu music/default.lua
+++ Sounds/_common menu music/default.lua
@@ -8,7 +8,8 @@ local songs = {
 	Gay    = "Mystical Wheelbarrow Journey",
 	Stars  = "Shooting Star - faux VRC6 remix",
 	Thonk  = "Da Box of Kardboard Too (feat Naoki vs ZigZag) - TaroNuke Remix",
-	Technique = "Quaq",
+--Technique = "Quaq",
+  Technique = "pump2nd",
 	SRPG9  = "SRPG9"
 }
 
